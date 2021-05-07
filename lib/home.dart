f0f4d2fa019a4d2b221a11e1ea24d9acf615ccae
Anshul <anshulpuransharma@gmail.com>
2021-05-07 20:57:45 +0530

    Fixed List View

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   //int _currIndex = 0;
+
+  final List<String> entries = <String>[
+    'Hospital Name',
+    'Hospital Name',
+    'Hospital Name',
+    'Hospital Name',
+    'Hospital Name'
+  ];
+  //final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,127 +23,27 @@ class Home extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: const <Widget>[
-            Card(
-              color: Color.fromRGBO(92, 132, 141, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
+        child: ListView.builder(
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                child: Card(
+                  color: (index % 2 == 0)
+                      ? Color.fromRGBO(92, 132, 141, 1)
+                      : Color.fromRGBO(157, 212, 243, 1),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.house,
+                      size: 56.0,
+                    ),
+                    title: Text('${entries[index]}'),
+                    //subtitle: Text('Address of Hospital'),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    //onTap: () {},
+                  ),
                 ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(157, 212, 243, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(92, 132, 141, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(157, 212, 243, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(92, 132, 141, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(157, 212, 243, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(92, 132, 141, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(157, 212, 243, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-            Card(
-              color: Color.fromRGBO(92, 132, 141, 1),
-              child: ListTile(
-                leading: Icon(
-                  Icons.house,
-                  size: 56.0,
-                ),
-                title: Text('Hospital Name'),
-                subtitle: Text('Address of Hospital'),
-                trailing: Icon(Icons.arrow_forward_ios),
-                //onTap: () {},
-              ),
-            ),
-          ],
-        ),
+              );
+            }),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
